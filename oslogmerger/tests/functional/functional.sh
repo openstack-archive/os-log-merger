6b87af03cd779097f09a5dfc5f69afd0af1e75e7
Miguel Angel Ajo <majopela@redhat.com>
2018-10-11 12:22:34 +0200

    Add functional test for var log messages and enhance functional script
    This test adds testing for var log messages format. In this commit we
    also enhance the functional.sh script to:
    
    1) make sed usage more compatible with other systems (--expression -> -e)
    2) Add color to output PASSED / FAILED
    3) Generate template files for non existing outputs
    4) Generate failed outputs for manual verification
    
    Change-Id: I1146b96fe9c91473c0127d0c47f890db1ddfb7b5

--- oslogmerger/tests/functional/functional.sh
+++ oslogmerger/tests/functional/functional.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 DATA=oslogmerger/tests/functional/data
 
 fail_func() {
-   echo FAILED test case $1 with output file $2
+   echo ${RED}FAILED${NC} test case $1 with output file $2
+   cp $3/out.log $4/$2.failed_output
+   echo $4/$2.failed_output created as reference
    rm -rf $3
    exit 1
 }
@@ -19,20 +25,35 @@ run_test() {
    shift 2
 
    # replace TDATA with the "in" directory for the test
-   PARAMS=$(echo $* | sed --expression=s%TDATA%${IN}%g)
+   PARAMS=$(echo $* | sed -e s%TDATA%${IN}%g)
 
    # run os-log-merger capturing the output, then compare to what we expected
    os-log-merger $PARAMS >$TMP/out.log
 
-   diff -u $OUT/$OUT_FILE $TMP/out.log || fail_func $TEST $OUT_FILE $TMP
+   if [ ! -f $OUT/$OUT_FILE ]; then
+        echo $RED
+        echo output file $OUT/$OUT_FILE does not exist, creating an example file:
+        echo ${OUT}/${OUT_FILE}.sample
+        echo $NC
+        cp $TMP/out.log ${OUT}/${OUT_FILE}.sample
+        fail_func $TEST $OUT_FILE $TMP
+   fi
 
-   echo PASSED test case $TEST / $OUT_FILE
+   diff -u $OUT/$OUT_FILE $TMP/out.log || fail_func $TEST $OUT_FILE $TMP $OUT
+
+   echo ${GREEN}PASSED${NC} test case $TEST / $OUT_FILE
    rm -rf $TMP
 
 }
 
 run_test 01-simple no-alias.log TDATA/metadata-agent.log TDATA/l3-agent.log
 run_test 01-simple alias.log  TDATA/metadata-agent.log:META TDATA/l3-agent.log:L3
+
+run_test 02-var-log-messages mixed.log TDATA/var_log_messages:messages \
+                                       TDATA/nova-api.log:napi
+run_test 02-var-log-messages mixed_ms.log TDATA/var_log_messages:messages \
+                                          TDATA/var_log_messages_ms:messages2 \
+                                          TDATA/nova-api.log:napi
 
 
 
