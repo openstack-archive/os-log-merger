222bb2a98478f8b3fc8786b8a69eefc873f94a55
Miguel Angel Ajo <majopela@redhat.com>
2018-10-11 07:40:21 +0200

    Create and enable funcional testing
    The funcional testing will include, per case, input files
    and output files, plus a simple bash script which makes
    sure the output stays as it should, and otherwise shows
    a diff and fails the case.
    
    Change-Id: I4c5f1143188f49c6a1448ca8918ab8b30710b865

---
+++ oslogmerger/tests/functional/functional.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+DATA=oslogmerger/tests/functional/data
+
+fail_func() {
+   echo FAILED test case $1 with output file $2
+   rm -rf $3
+   exit 1
+}
+
+run_test() {
+
+   TEST=$1
+   OUT_FILE=$2
+   IN=$DATA/$TEST/in
+   OUT=$DATA/$TEST/out
+   TMP=$(mktemp -d)
+
+   shift 2
+
+   # replace TDATA with the "in" directory for the test
+   PARAMS=$(echo $* | sed --expression=s%TDATA%${IN}%g)
+
+   # run os-log-merger capturing the output, then compare to what we expected
+   os-log-merger $PARAMS >$TMP/out.log
+
+   diff -u $OUT/$OUT_FILE $TMP/out.log || fail_func $TEST $OUT_FILE $TMP
+
+   echo PASSED test case $TEST / $OUT_FILE
+   rm -rf $TMP
+
+}
+
+run_test 01-simple no-alias.log TDATA/metadata-agent.log TDATA/l3-agent.log
+run_test 01-simple alias.log  TDATA/metadata-agent.log:META TDATA/l3-agent.log:L3
+
+
+
+
